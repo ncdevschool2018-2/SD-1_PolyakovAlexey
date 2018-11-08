7eef983387d873ad9a982271cb1d306db66eb26b
Alexey Polyakov <atfordemail@gmail.com>
2018-11-08 20:29:34 +0300

    Added Constans instead of enums

---
+++ sd4parent/frontend/src/app/modules/shared/models/Constans.ts
@@ -0,0 +1,22 @@
+export class Constans {
+  statuses: {
+    open: "open",
+    inProgress: "in progress",
+    readyForTest: "ready for test"
+  };
+
+  priorities: {
+    blocker: "blocker",
+    critical: "critical",
+    major: "major",
+    normal: "normal",
+    minor: "minor"
+  };
+
+  userRoles: {
+    admin: "admin",
+    projectManager: "project manager",
+    developer: "developer",
+    qualityAssurance: "quality assurance"
+  };
+}
