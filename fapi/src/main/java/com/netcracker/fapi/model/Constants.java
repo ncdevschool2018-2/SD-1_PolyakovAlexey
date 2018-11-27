e7a9d0c6633774511bd6938bf056a1f9abd3e103
Alexey <cnbdvfrrufhtn1>
2018-11-27 16:42:40 +0300

    Time of token has been changed to 3 hours

--- fapi/src/main/java/com/netcracker/fapi/model/Constants.java
+++ fapi/src/main/java/com/netcracker/fapi/model/Constants.java
@@ -3,7 +3,7 @@ package com.netcracker.fapi.model;
 
 public class Constants {
 
-    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 5 * 60 * 60;
+    public static final long ACCESS_TOKEN_VALIDITY_SECONDS = 3 * 60 * 60;
     public static final String SIGNING_KEY = "atford";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
